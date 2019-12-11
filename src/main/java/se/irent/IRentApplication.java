907b8a1c2e92788a1041bad8087c4b0480298abd
philophobia <526567789@qq.com>
2019-12-11 22:07:09 +0800

    初始化SpringBoot项目

---
+++ src/main/java/se/irent/IRentApplication.java
@@ -0,0 +1,13 @@
+package se.irent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IRentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IRentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/se/irent/IRentApplicationTests.java
@@ -0,0 +1,13 @@
+package se.irent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IRentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
