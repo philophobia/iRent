019ca87f1bfd3d40e1cf415661f9fa75c1b91f4d
philophobia <526567789@qq.com>
2019-12-19 13:36:33 +0800

    改名

--- src/main/java/se/irent/dao/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package se.irent.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import se.irent.entity.Order;
-
-public interface OrderRepository extends JpaRepository<Order, String> {
-}
